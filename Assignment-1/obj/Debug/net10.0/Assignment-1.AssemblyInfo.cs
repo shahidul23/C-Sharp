babb8fcee5000c96d1acaaf4419d6b0fe92125bc
shahidul islam <shahidul239.si@gmail.com>
2026-08-11 10:57:50 +0600

    update

--- Assignment-1/obj/Debug/net10.0/Assignment-1.AssemblyInfo.cs
+++ Assignment-1/obj/Debug/net10.0/Assignment-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baea587fa3d2d9a5b19884ec3b65260ea90e1d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a4d16bcab7a8ae52733668186575715e561e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
